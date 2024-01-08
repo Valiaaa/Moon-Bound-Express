7c940a6bcd468ed0dc27f81ad26f1cea5ccce8d3
Valiaaa <valialcc@gmail.com>
2024-01-08 12:11:48 -0500

    update

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 $(document).ready(function(){
 
-$("#indexdoor").click(function(){
-    var output;
-    var result = Array(6);
-    result[0] = "osmanthus_honey_brew";
-    result[1] = "under_the_umbrella";
-    result[2] = "unrequitted_love";
-    result[3] = "troubled_by_the_moon";
-    result[4] = "well-intentioned";
-    result[5] = "four_hundred_dollars";
+// $("#indexdoor").click(function(){
+//     var output;
+//     var result = Array(6);
+//     result[0] = "osmanthus_honey_brew";
+//     result[1] = "under_the_umbrella";
+//     result[2] = "unrequitted_love";
+//     result[3] = "troubled_by_the_moon";
+//     result[4] = "well-intentioned";
+//     result[5] = "four_hundred_dollars";
 
-    var Num = Math.floor(Math.random() * 6);
-    var output = result [Num];
-    // console.log(output);
-    window.location.href = "" + output + ".html";
+//     var Num = Math.floor(Math.random() * 6);
+//     var output = result [Num];
+//     // console.log(output);
+//     window.location.href = "" + output + ".html";
+// });
+
+// Retrieve the last room from localStorage
+var lastRoom = localStorage.getItem('lastRoom');
+
+$("#indexdoor").click(function() {
+    var result = [
+        "osmanthus_honey_brew",
+        "under_the_umbrella",
+        "unrequitted_love",
+        "troubled_by_the_moon",
+        "well-intentioned",
+        "four_hundred_dollars"
+    ];
+
+    var Num;
+    do {
+        Num = Math.floor(Math.random() * result.length);
+    } while (Num === parseInt(lastRoom));
+
+    lastRoom = Num;
+
+    // Store the last room in localStorage
+    localStorage.setItem('lastRoom', lastRoom);
+
+    var output = result[Num];
+    window.location.href = output + ".html";
 });
+
+// Output the array
+console.log(result);
+
 
 $(".cross").click(function(){
     $('.blogholder').hide();
